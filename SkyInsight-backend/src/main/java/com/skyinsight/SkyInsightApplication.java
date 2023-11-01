3f46d73f7bf1182832821c61509aa0fae4f8f687
rahulvikhe1 <rahulvikhe1@gmail.com>
2023-11-02 00:08:54 +0530

    Add Spring Boot backend setup

---
+++ SkyInsight-backend/src/main/java/com/skyinsight/SkyInsightApplication.java
@@ -0,0 +1,13 @@
+package com.skyinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyInsightApplication.class, args);
+	}
+
+}
---
+++ SkyInsight-backend/src/test/java/com/skyinsight/SkyInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
